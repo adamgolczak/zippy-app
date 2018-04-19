6ebe9ce0502e41f78b01362129c14507b715d060
Adam Golczak <adam.golczak@gmail.com>
2018-04-19 23:25:26 +0200

    Initial Commit

---
+++ zippy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zippy
+//
+//  Created by Adam Golczak on 19/04/2018.
+//  Copyright © 2018 Adam Golczak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
